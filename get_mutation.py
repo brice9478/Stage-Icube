a128d67d3461f05bbf2620bbd4a900dd9928bef8
Brice <brice.laurent@epitech.eu>
2024-11-12 12:57:15 +0100

    [/] -h, arguments when executed. Probably final version.

--- get_mutation.py
+++ get_mutation.py
@@ -69,7 +69,7 @@ def check_multiple_pdi(pdi, data, positions, total_data, count):
 def divide_and_conquer(total_data, count, error, rps):
     url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=clinvar&term=" + str(total_data[count][0]) + "&retmode=fasta"
     try:
-        params = {'api_key': "api key"} # /!\
+        params = {'api_key': argv[1]} # /!\
         response = requests.get(url, timeout=5, params=params)
     except:
         if error >= 5:
@@ -86,7 +86,7 @@ def divide_and_conquer(total_data, count, error, rps):
     data = re.sub(r"[\n \t]", '', data)
     data = re.split(r"[><]", data)
     print("DATA SUCCESSFULLY CREATED", str(total_data[count][0]))
-    for i in range(len(data) - 1):
+    for i in range(len(data)):
         if data[i] == '':
             del data[i]
             if i == len(data) - 2:
@@ -113,7 +113,8 @@ def good_or_evil(total_data, count):
             url = url + ", "
     url = url + "&retmode=fasta"
     print(url)
-    response = requests.get(url)
+    params = {'api_key': argv[1]}
+    response = requests.get(url, timeout=10, params=params)
     if len(rps) >= 10:
         del rps[0]
     data = ''.join(response.text)
@@ -124,11 +125,14 @@ def good_or_evil(total_data, count):
             del data[i]
             if i == len(data) - 2:
                 break
-    positions = findex_description(data, "germline_classification") #yo what ??? RESULT ['P15056', ['id', '3257758', '/description', 'position(s)', ['140753335', '140753336'], 'deletion(s)', ['A', 'C'], 'insertion(s)', ['T', 'T']], ['id', '280033', 'Pathogenic', 'position(s)', '140753338', 'deletion(s)', 'G', 'insertion(s)', 'C']...
+    positions = findex_description(data, "germline_classification")
     for glc in range(len(positions)):
         if len(total_data[count][glc + 1]) > 2:
             continue
-        total_data[count][glc + 1].append(data[positions[glc] + 2])
+        if data[positions[glc] + 2] == "/description": #sometimes, germline_classification doesn't have a description. Ex : id 3257758
+            total_data[count][glc + 1].append("Aknown")
+        else:
+            total_data[count][glc + 1].append(data[positions[glc] + 2])
 
     positions = findexstr(data, "canonical_spdi")
     pdi = []
@@ -162,7 +166,26 @@ def timer(count_time):
         count_time[0] += 0.1
 
 
-if len(argv) == 1:
+if len(argv) == 1 or len(argv) > 3:
+    print("retry with -h for sole argument")
+    exit(84)
+if argv[1] == "-h":
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    print("This program needs 2 arguments :")
+    print("python3 get_mutation.py [api key] [file.txt]")
+    print("api key : find it on https://account.ncbi.nlm.nih.gov/settings/")
+    print("file.txt : a file that contains a list of human genes' accession all separated by a \\n")
+    print("The program will generate a pickle file named gen_mutation.pickle.\nIf a file with the name \"gen_mutation.pickle\" already exists, new information will be added to the already existing one.")
+    print("note : if no file is given as argument, the program will automatically use the default excel file GenoDENT_genes.xlsx")
+    print("Info : if you have a doubt on any result try :")
+    print("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=clinvar&term=[?]&retmode=fasta \n\\--> replace [?] by the gene's accession to get the id")
+    print("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=clinvar&id=[?]&retmode=fasta \n\\--> replace [?] by the id to get information on the mutation")
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    exit(0)
+if len(argv) == 2:
+    if not os.path.exists("GenoDENT_genes.xlsx"):
+        print("File GenoDENT_genes.xlsx not found.")
+        exit(84)
     excel_file = load_workbook("GenoDENT_genes.xlsx")
     content = excel_file.active
     total_data = []
@@ -172,9 +195,18 @@ if len(argv) == 1:
     else:
         for i in range(len(content['B']) - 1):
             total_data.append([content['B' + str(i + 2)].value])
-else:
-    print("This program needs a simple array of genes' accession to work.")
-    exit(0)
+if len(argv) == 3:
+    total_data = []
+    if not os.path.exists(argv[2]):
+        print("The file named", argv[2], "can't be found.")
+        exit(84)
+    if os.path.exists("gen_mutation.pickle"):
+        print("/!\\ gen_mutation.pickle already exists. New informations will be added to the existing one. /!\\")
+        with open("gen_mutation.pickle", "rb") as file:
+            total_data = pickle.load(file)
+    with open(argv[2], 'r') as file:
+        for line in file:
+            total_data.append([line.strip()])
 
 
 
@@ -184,17 +216,15 @@ count_time = [0]
 threading.Thread(target=timer, daemon=True, args=(count_time,)).start()
 rps = []
 print("threads", threading.active_count())
-for count in range(len(total_data) - 1):
+for count in range(len(total_data)):
     print(len(total_data[count]), total_data[count])
     if len(total_data[count]) > 1: # > 1
         continue
     print("time :", count_time[0])
     rps.append(count_time[0])
     threading.Thread(target=divide_and_conquer, daemon=True, args=(total_data, count, 0, rps)).start()
-    # print("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy RPS :", rps)
     while count_time[0] - rps[0] <= 1.0 or len(rps) >= 10:
         time.sleep(0.1)
-        # print("============================== RPS :", rps)
     with open("gen_mutation.pickle", "wb") as file:
         pickle.dump(total_data, file)
 print(total_data)
@@ -210,7 +240,7 @@ with open("gen_mutation.pickle", "wb") as file:
 
 count_time[0] = 0
 rps = []
-for count in range(len(total_data) - 1):
+for count in range(len(total_data)):
     if len(total_data[count]) <= 1:
         continue
     rps.append(count_time[0])
@@ -226,4 +256,4 @@ while threading.active_count() > 2 and iferror < 10:    #security mesure
 time.sleep(3)
 print(total_data)
 with open("gen_mutation.pickle", "wb") as file:
-        pickle.dump(total_data, file)
+    pickle.dump(total_data, file)
