5f387e78012dc83c2a47f0255dc1d37244dcdb1e
Brice <brice.laurent@epitech.eu>
2024-11-14 15:09:05 +0100

    [/] -h, arguments, optimisation. Fast, but might miss some sequences (connection error or whatever)

--- get_seq.py
+++ get_seq.py
@@ -5,41 +5,30 @@ import threading
 import time
 from Bio import SeqIO
 from io import StringIO
+from sys import argv
 
-# ------------- get total_data -------------
-with open("orthologues.pickle", "rb") as file:
-    total_data = pickle.load(file)
-
-if not os.path.exists("gen_seq.pickle"):
-    gen_seq = []
-    counter = 0
-    for h_gene in range(0, len(total_data["genes"])):
-        for ortho in range(0, len(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]])):
-            for access in range(0, len(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]][ortho]["specie"][2])):
-                gen_seq.append(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]][ortho]["specie"][2][access])
-                counter += 1
-    gen_seq = list(set(gen_seq)) #enlève les doublons
-    with open("gen_seq.pickle", "wb") as file:
-        pickle.dump(gen_seq, file)
-else:
-    with open("gen_seq.pickle", "rb") as file:
-        gen_seq = pickle.load(file)
-
-print(len(gen_seq))
-print(gen_seq[len(gen_seq) - 1])
-
-sequences = gen_seq
-
-def get_request(gen_seq, i, max):
+def get_request(gen_seq, i, max, error):
     url = "https://rest.uniprot.org/uniprotkb/search?query=("
     for k in range(0, max):
-        print(i + k, gen_seq[i + k])
         url = url + "accession:"
         url = url + gen_seq[i + k]
         if k < max - 1:
             url = url + "+OR+"
     url = url + ")&format=fasta"
-    response = requests.get(url)
+    try:
+        response = requests.get(url, timeout=10)
+    except:
+        if error >= 5:
+            total_data[i].append(["Aknown"])
+            off = 0
+            while rps[off][1] != False:
+                off += 1
+            rps[off][1] = count_time[0]
+            print("Request : all attempts failed.")
+            return
+        print("/!\ ---------> loop ? Error connection. Request sent again.", str(total_data[i][0]))
+        get_request(total_data, i, max, error + 1)
+        return
     data = ''.join(response.text)
     Seq = StringIO(data)
     protSeq = list(SeqIO.parse(Seq,'fasta'))
@@ -66,25 +55,148 @@ def get_request(gen_seq, i, max):
                 sequences[i + backup] = sequences[i + backup].split(":")
                 print(sequences[i + backup])
             time.sleep(0.1)
+    off = 0
+    while rps[off][1] != False:
+        off += 1
+    rps[off][1] = count_time[0]
+
+def retry_aknowns(gen_seq, i, error):
+    print(gen_seq[i], "at", i)
+    url = "http://rest.uniprot.org/unisave/" + sequences[i][0] + "?format=fasta"
+    try:
+        response = requests.get(url, timeout=10)
+    except:
+        if error >= 5:
+            total_data[i].append(["Aknown"])
+            off = 0
+            while rps[off][1] != False:
+                off += 1
+            rps[off][1] = count_time[0]
+            print("Request : all attempts failed.")
+            return
+        print("/!\ ---------> loop ? Error connection. Request sent again.", str(total_data[i][0]))
+        get_request(total_data, i, max, error + 1)
+        return
+    data = ''.join(response.text)
+    Seq = StringIO(data)
+    protSeq = list(SeqIO.parse(Seq,'fasta'))
+    try:
+        sequences[i][1] = str(protSeq[0].seq)
+    except:
+        sequences[i][1] = "Aknown"
+        print(sequences[i][0], ": sequence couldn't be found.")
+    off = 0
+    while rps[off][1] != False:
+        off += 1
+    rps[off][1] = count_time[0]
 
 
+def timer(count_time, start_time):
+    count_second = 0
+    while True:
+        if count_time[0] >= count_second + 1:
+            count_second += 1
+        count_time[0] = time.time() - start_time[0]
+
+
+
+if len(argv) > 2:
+    print("retry with -h for sole argument")
+    exit(84)
+if len(argv) == 2 and argv[1] == "-h":
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    print("python3 get_seq.py [file.txt]")
+    print("file.txt : a file that contains a list of human genes' accession all separated by a \\n")
+    print("The program will generate a pickle file named sequences.pickle.\nIf a file with the name \"sequences.pickle\" already exists, new information will be added to the already existing one.")
+    print("note : if no file is given as argument, the program will automatically use the default orthologues.pickle")
+    print("Info : if you have a doubt on any result try :")
+    print("https://rest.uniprot.org/uniprotkb/[?].fasta \n\\--> replace [?] by the gene's accession")
+    print("-----------------------------------------------------------------------------------------------------------------------")
+    exit(0)
+if len(argv) == 1:
+    if not os.path.exists("orthologues.pickle"):
+        print("file orthologues.pickle not found")
+    with open("orthologues.pickle", "rb") as file:
+        total_data = pickle.load(file)
+    if not os.path.exists("gen_seq.pickle"):
+        gen_seq = []
+        counter = 0
+        for h_gene in range(0, len(total_data["genes"])):
+            for ortho in range(0, len(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]])):
+                for access in range(0, len(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]][ortho]["specie"][2])):
+                    gen_seq.append(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]][ortho]["specie"][2][access])
+                    counter += 1
+        gen_seq = list(set(gen_seq)) #enlève les doublons
+        with open("gen_seq.pickle", "wb") as file:
+            pickle.dump(gen_seq, file)
+    else:
+        with open("gen_seq.pickle", "rb") as file:
+            gen_seq = pickle.load(file)
+if len(argv) == 2:
+    if not os.path.exists(argv[1]):
+        gen_seq = []
+    if not os.path.exists(argv[2]):
+        print("The file named", argv[2], "can't be found.")
+        exit(84)
+    if os.path.exists("sequences.pickle"):
+        print("/!\\ sequences.pickle already exists. New informations will be added to the existing one. /!\\")
+        with open("sequences.pickle", "rb") as file:
+            gen_seq = pickle.load(file)
+    with open(argv[2], 'r') as file:
+        for line in file:
+            gen_seq.append([line.strip()])
+
+
+
+start_time = [time.time()]
+count_time = [0]
+threading.Thread(target=timer, daemon=True, args=(count_time, start_time)).start()
+rps = []
+# ------------- get total_data -------------
+
+print(len(gen_seq))
+print(gen_seq[len(gen_seq) - 1])
+
+sequences = gen_seq
+i = 0
 for i in range(0, len(gen_seq), 25):
     max = 25
-    threading.Thread(target=get_request, daemon=False, args=(gen_seq, i, max)).start()
-    while threading.active_count() >= 31:
+    print(i)
+    rps.append([count_time[0], False])
+    threading.Thread(target=get_request, daemon=True, args=(gen_seq, i, max, 0)).start()
+    while len(rps) >= 30:
+        if rps[0][1] != False and count_time[0] - rps[0][1] > 1:
+            break
         time.sleep(0.1)
-threading.Thread(target=get_request, daemon=False, args=(gen_seq, i, len(gen_seq) - i - 1)).start()
-time.sleep(10)
+    try:
+        if rps[0][1] != False:
+            del rps[0]
+    except:
+        continue
+print("Number I :", i, "len gen seq :", len(gen_seq))
+threading.Thread(target=get_request, daemon=True, args=(gen_seq, i, len(gen_seq) - i + 1)).start()
+iferror = 0
+while threading.active_count() > 2 and iferror < 10:    #security mesure --> waiting for all previous threads to finish
+    iferror = iferror + 1
+    print("threads:", threading.active_count())
+    time.sleep(1)
 
-for retry in range(0, len(gen_seq) - 1):
-    if sequences[retry][1] == "Aknown":
-        print(gen_seq[retry])
-        url = "http://rest.uniprot.org/unisave/" + sequences[retry][0] + "?format=fasta"
-        response = requests.get(url)
-        data = ''.join(response.text)
-        Seq = StringIO(data)
-        protSeq = list(SeqIO.parse(Seq,'fasta'))
-        sequences[retry][1] = str(protSeq[0].seq)
+start_time[0] = time.time()
+count_time[0] = 0
+rps = []
+for i in range(0, len(gen_seq)):
+    if sequences[i][1] == "Aknown":
+        rps.append([count_time[0], False])
+        threading.Thread(target=retry_aknowns, daemon=True, args=(gen_seq, i, 0)).start()
+        while len(rps) >= 10:
+            if rps[0][1] != False and count_time[0] - rps[0][1] > 1:
+                break
+            time.sleep(0.1)
+    try:
+        if rps[0][1] != False:
+            del rps[0]
+    except:
+        continue
 
 with open("sequences.pickle", "wb") as file:
     pickle.dump(sequences, file)
