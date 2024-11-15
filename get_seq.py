380faa8a7457f03512d01133f04f8caadc255584
Brice <brice.laurent@epitech.eu>
2024-11-15 11:27:21 +0100

    [/] problem with file given as argument corrected. Final version.

--- get_seq.py
+++ get_seq.py
@@ -11,7 +11,10 @@ def get_request(gen_seq, i, max, error):
     url = "https://rest.uniprot.org/uniprotkb/search?query=("
     for k in range(0, max):
         url = url + "accession:"
-        url = url + gen_seq[i + k]
+        if len(gen_seq[i + k]) == 2:
+            url = url + str(gen_seq[i + k][0])
+        else:
+            url = url + str(gen_seq[i + k])
         if k < max - 1:
             url = url + "+OR+"
     url = url + ")&format=fasta"
@@ -40,6 +43,8 @@ def get_request(gen_seq, i, max, error):
     except:
         print("-----Error. Requesting backup-----")
         for backup in range(0, max):
+            if len(sequences[i + backup]) == 2:
+                continue
             url = "http://rest.uniprot.org/unisave/" + gen_seq[i + backup] + "?format=fasta"
             response = requests.get(url)
             data = ''.join(response.text)
@@ -53,15 +58,17 @@ def get_request(gen_seq, i, max, error):
                 print("-----/!\\ Problematic element : ", gen_seq[i + backup], "/!\\-----")
                 sequences[i + backup] = sequences[i + backup] + ":" + "Aknown"
                 sequences[i + backup] = sequences[i + backup].split(":")
-                print(sequences[i + backup])
             time.sleep(0.1)
-    off = 0
-    while rps[off][1] != False:
-        off += 1
-    rps[off][1] = count_time[0]
+    try:
+        off = 0
+        while rps[off][1] != False:
+            off += 1
+        rps[off][1] = count_time[0]
+    except:
+        return
 
 def retry_aknowns(gen_seq, i, error):
-    print(gen_seq[i], "at", i)
+    print(sequences[i][0], "at", i)
     url = "http://rest.uniprot.org/unisave/" + sequences[i][0] + "?format=fasta"
     try:
         response = requests.get(url, timeout=10)
@@ -85,10 +92,13 @@ def retry_aknowns(gen_seq, i, error):
     except:
         sequences[i][1] = "Aknown"
         print(sequences[i][0], ": sequence couldn't be found.")
-    off = 0
-    while rps[off][1] != False:
-        off += 1
-    rps[off][1] = count_time[0]
+    try:
+        off = 0
+        while rps[off][1] != False:
+            off += 1
+        rps[off][1] = count_time[0]
+    except:
+        return
 
 
 def timer(count_time, start_time):
@@ -118,7 +128,7 @@ if len(argv) == 1:
         print("file orthologues.pickle not found")
     with open("orthologues.pickle", "rb") as file:
         total_data = pickle.load(file)
-    if not os.path.exists("gen_seq.pickle"):
+    if not os.path.exists("sequences.pickle"):
         gen_seq = []
         counter = 0
         for h_gene in range(0, len(total_data["genes"])):
@@ -127,24 +137,21 @@ if len(argv) == 1:
                     gen_seq.append(total_data["genes"][h_gene][list(total_data["genes"][h_gene].keys())[0]][ortho]["specie"][2][access])
                     counter += 1
         gen_seq = list(set(gen_seq)) #enlève les doublons
-        with open("gen_seq.pickle", "wb") as file:
-            pickle.dump(gen_seq, file)
     else:
-        with open("gen_seq.pickle", "rb") as file:
+        with open("sequences.pickle", "rb") as file:
             gen_seq = pickle.load(file)
 if len(argv) == 2:
+    gen_seq = []
     if not os.path.exists(argv[1]):
-        gen_seq = []
-    if not os.path.exists(argv[2]):
-        print("The file named", argv[2], "can't be found.")
+        print("The file named", argv[1], "can't be found.")
         exit(84)
     if os.path.exists("sequences.pickle"):
         print("/!\\ sequences.pickle already exists. New informations will be added to the existing one. /!\\")
         with open("sequences.pickle", "rb") as file:
             gen_seq = pickle.load(file)
-    with open(argv[2], 'r') as file:
+    with open(argv[1], 'r') as file:
         for line in file:
-            gen_seq.append([line.strip()])
+            gen_seq.append(line.strip())
 
 
 
@@ -152,15 +159,12 @@ start_time = [time.time()]
 count_time = [0]
 threading.Thread(target=timer, daemon=True, args=(count_time, start_time)).start()
 rps = []
-# ------------- get total_data -------------
-
-print(len(gen_seq))
-print(gen_seq[len(gen_seq) - 1])
-
 sequences = gen_seq
 i = 0
 for i in range(0, len(gen_seq), 25):
     max = 25
+    if i + max >= len(gen_seq):
+        break
     print(i)
     rps.append([count_time[0], False])
     threading.Thread(target=get_request, daemon=True, args=(gen_seq, i, max, 0)).start()
@@ -174,7 +178,7 @@ for i in range(0, len(gen_seq), 25):
     except:
         continue
 print("Number I :", i, "len gen seq :", len(gen_seq))
-threading.Thread(target=get_request, daemon=True, args=(gen_seq, i, len(gen_seq) - i + 1)).start()
+threading.Thread(target=get_request, daemon=True, args=(gen_seq, i, len(gen_seq) - i, 0)).start()
 iferror = 0
 while threading.active_count() > 2 and iferror < 10:    #security mesure --> waiting for all previous threads to finish
     iferror = iferror + 1
@@ -200,3 +204,4 @@ for i in range(0, len(gen_seq)):
 
 with open("sequences.pickle", "wb") as file:
     pickle.dump(sequences, file)
+exit(0)
